cc3acebf31bd2126e7b9d7abfbf407c8613964d5
Benjamin HARTMANN <jpnbenj@gmail.com>
2021-12-23 15:25:21 +0100

    Part 3 - test 2

--- 3 - MongoDB and Mongoose/myApp.js
+++ 3 - MongoDB and Mongoose/myApp.js
@@ -3,7 +3,17 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods:[String]
+});
+
 let Person;
+
+Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
