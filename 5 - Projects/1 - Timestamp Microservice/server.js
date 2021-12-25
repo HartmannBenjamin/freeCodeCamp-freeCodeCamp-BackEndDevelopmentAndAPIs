375c18aa20b456dd68ef42b7817c1e78d3019e65
Benjamin HARTMANN <jpnbenj@gmail.com>
2021-12-25 11:44:27 +0100

    Part 5 - project 1

---
+++ 5 - Projects/1 - Timestamp Microservice/server.js
@@ -0,0 +1,46 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:param", function (req, res) {
+  let param = req.params.param;
+  let dateParam = null;
+
+  if (/^\d{4}[./-]\d{2}[./-]\d{2}$/.test(req.params.param)) {
+    dateParam = new Date(param);
+  } else {
+    dateParam = new Date(param * 1000);
+  }
+
+  let resp = {};
+  resp.unix = dateParam.getTime();
+  resp.utc = dateParam.toUTCString();
+
+  res.json(resp);
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
